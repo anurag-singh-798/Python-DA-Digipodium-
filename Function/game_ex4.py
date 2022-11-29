cd29f756dd8c46afe75c8da53b61e52139a65e23
Anuragsingh <anuragsinghasb798@gmail.com>
2022-11-29 18:43:57 +0530

    game4

---
+++ Function/game_ex4.py
@@ -0,0 +1,37 @@
+import pgzrun
+
+WIDTH = 1200
+HEIGHT = 600
+
+scr = 0
+
+def gamescr(title, bgcolor='gray', info="Play the game"):
+      screen.fill(bgcolor)
+      screen.draw.text(title, center=(WIDTH/2, HEIGHT/2),
+         fontsize=100, color='white', align='center')
+      screen.draw.text(info, center=(WIDTH/2,HEIGHT/2+100),
+         fontsize=50, color='white', align='center')
+
+
+
+
+def draw():
+    if scr == 0:
+      gamescr('Amazing Game', 'black', 'Press space to start')
+    elif scr == 1:
+        gamescr(bgcolor='green', title='Game is running')
+    elif scr == 2:
+        gamescr('Game Over', info='Press enter to restart')
+    
+
+def update():
+    global scr
+    if keyboard.SPACE and scr == 0:
+        scr = 1
+    if keyboard.ESCAPE and scr == 1:
+        scr = 2
+    if keyboard.RETURN and scr == 2:
+        scr == 0
+    print(scr)
+
+pgzrun.go()
