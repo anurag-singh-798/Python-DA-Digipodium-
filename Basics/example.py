bb2419e57aa9e2c8964e30d6fa1a80ebd5e705ad
Anuragsingh <anuragsinghasb798@gmail.com>
2022-11-29 13:00:33 +0530

    example

---
+++ Basics/example.py
@@ -0,0 +1,9 @@
+from unicodedata import name
+
+
+name = input("Enter your name : ")
+print('Welcome', name)
+amount = input("How much tea will you take : ")
+print('Why would u want', amount, 'Tea')
+
+
