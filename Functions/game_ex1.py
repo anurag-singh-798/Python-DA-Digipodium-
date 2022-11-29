a0c5b3d8a08bbc7da2ecbfaa5ef02965f6e10ffd
Anuragsingh <anuragsinghasb798@gmail.com>
2022-11-29 11:36:20 +0530

    game

---
+++ Functions/game_ex1.py
@@ -0,0 +1,22 @@
+import pgzrun
+
+HEIGHT = 600
+WIDTH = 800
+p = Actor('ironman', (100, 100))
+c = Actor('cookie_img')
+
+def draw():
+    screen.fill('white')
+    p.draw()
+    c.draw()
+    # print('drawing')
+
+def update():
+    # print('updating')
+    p.x -= 3
+    p.angle = -10
+    if p.x < 0: # agar player left side se bahar jaye toh
+        p.x = WIDTH 
+    print(p.x, p.y)
+
+pgzrun.go()
