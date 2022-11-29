b0b2c1e9efbeed3debe8a64025bb89949805c509
Anuragsingh <anuragsinghasb798@gmail.com>
2022-11-29 12:54:59 +0530

    basic

--- Basics/area.py
+++
@@ -1,9 +0,0 @@
-# area of circle using its circumference
-
-c = float(input("Enter circumference : "))
-
-r = c/(2*3.14)
-area = 3.14 * r * r
-
-print("Radius : ", r)
-print("Area : ", area)
--- Basics/example.py
+++
@@ -1,9 +0,0 @@
-from unicodedata import name
-
-
-name = input("Enter your name : ")
-print('Welcome', name)
-amount = input("How much tea will you take : ")
-print('Why would u want', amount, 'Tea')
-
-
